c3cf932b2377ee7821a988a87c7d1aa761d39337
legendarydeveloper919 <peterandersen919@gmail.com>
2024-02-28 10:03:19 -0800

    fixed errors

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,9 +3,4 @@ class RecipesController < ApplicationController
     recipes = Recipe.all.map { |recipe| recipe.as_json.merge(ingredients: recipe.ingredients) }
     render json: recipes
   end
-
-  private
-  def recipe_params
-    params.require(:recipe).permit(:recipe_name, :ingredients, :instructions, :description, :user_id, :public)
-  end
 end
