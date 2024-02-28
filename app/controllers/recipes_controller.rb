111046526e333f723fa714d95327ceaf5293d1a2
legendarydeveloper919 <peterandersen919@gmail.com>
2024-02-28 10:00:33 -0800

    fixed errors

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,15 +4,6 @@ class RecipesController < ApplicationController
     render json: recipes
   end
 
-  def create
-    recipe = Recipe.create(recipe_params)
-    if recipe.valid?
-      render json: recipe
-    else
-      render json: recipe.errors, status: 422
-    end
-  end
-
   private
   def recipe_params
     params.require(:recipe).permit(:recipe_name, :ingredients, :instructions, :description, :user_id, :public)
