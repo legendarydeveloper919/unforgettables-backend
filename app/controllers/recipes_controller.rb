e484aba9cee0b683a0658c874866a1339fac1241
legendarydeveloper919 <peterandersen919@gmail.com>
2024-02-28 09:52:45 -0800

    adds read functionality and request specs

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,20 @@
 class RecipesController < ApplicationController
+  def index
+    recipes = Recipe.all.map { |recipe| recipe.as_json.merge(ingredients: recipe.ingredients) }
+    render json: recipes
+  end
+
+  def create
+    recipe = Recipe.create(recipe_params)
+    if recipe.valid?
+      render json: recipe
+    else
+      render json: recipe.errors, status: 422
+    end
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:recipe_name, :ingredients, :instructions, :description, :user_id, :public)
+  end
 end
