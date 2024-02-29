c82cf20c12ee1a94426ef202c26a1c7a8fcdbb03
legendarydeveloper919 <peterandersen919@gmail.com>
2024-02-29 14:20:12 -0800

    made changes to validations test to reflect schema and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@ recipes = [
   {
     recipe_name: "Spaghetti Carbonara",
     description: "A classic Italian pasta dish with creamy sauce.",
-    ingredients: ["Spaghetti", "Eggs", "Pancetta", "Parmesan Cheese", "Black Pepper"],
+    ingredients: "Spaghetti, Eggs, Pancetta, Parmesan Cheese ,Black Pepper",
     instructions: "Cook spaghetti according to package instructions. In a separate pan, cook pancetta until crispy. In a bowl, whisk eggs, grated Parmesan cheese, and black pepper. Once spaghetti is cooked, drain and add to the pan with pancetta. Turn off heat, add egg mixture, and toss until coated and creamy. Serve immediately.",
     public: true
   },
   {
     recipe_name: "Chicken Tikka Masala",
     description: "A popular Indian curry dish with tender chicken in a creamy tomato sauce.",
-    ingredients: ["Chicken", "Yogurt", "Tomato Sauce", "Onion", "Garlic", "Ginger", "Spices"],
+    ingredients: "Chicken, Yogurt, Tomato Sauce, Onion, Garlic, Ginger, Spices",
     instructions: "Marinate chicken in yogurt and spices. Grill or bake until cooked through. In a separate pan, sauté onions, garlic, and ginger until soft. Add tomato sauce and spices, then simmer. Add cooked chicken to the sauce and simmer until flavors meld. Serve with rice and naan.",
     
     public: true
@@ -21,7 +21,7 @@ recipes = [
   {
     recipe_name: "Avocado Toast",
     description: "A simple and nutritious breakfast or snack option.",
-    ingredients: ["Bread", "Avocado", "Lemon Juice", "Salt", "Red Pepper Flakes"],
+    ingredients: "Bread, Avocado, Lemon Juice, Salt, Red Pepper Flakes",
     instructions: "Toast bread until golden brown. Mash avocado with lemon juice and salt. Spread avocado mixture on toast. Sprinkle with red pepper flakes. Enjoy!",
     
     public: false
